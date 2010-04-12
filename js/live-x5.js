e625ebf3153c15c0773ef14186c8c69bd1bda6f5
darobin <robin@berjon.com>
2010-04-12 11:30:34 +0200

    Outline; some style; some tooling

---
+++ js/live-x5.js
@@ -0,0 +1,31 @@
+
+(function ($, GLOBAL) {
+    var X5 = function () {};
+    X5.prototype = {
+        run:    function () {
+            $("head").append($("<link/>", { rel: "stylesheet", media: "all", href: "../css/live-x5.css" }));
+            var tits = document.title.split(":");
+            $("article").prepend("<h1></h1>")
+                        .find("h1")
+                            .append(tits[0] + ":")
+                            .append("<br/>")
+                            .append(tits[1]);
+            // fix headers?
+            // toc?
+            this.wcount();
+            $(".remove").remove();
+        },
+        wcount:    function () {
+            var txt = $("body")[0].textContent.replace(/\s+/g, " ");
+            var signs = txt.length;
+            var words = txt.split(" ").length;
+            $("<div id='wcount'>" + signs + "/" + words + "</div>").appendTo($("article"));
+        },
+    };
+    
+    GLOBAL.X5 = new X5();
+})(jQuery, this);
+
+jQuery(function () {
+    X5.run();
+});
